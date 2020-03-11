3ab0fdcca8845dc398989c3f4970bafef7e85c54
cckk00 <cckk00@126.com>
2020-03-11 10:47:03 +0800

    by LZD

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 window.onload = function () {
   console.log('loaded');
-
+  console.log('hello');
   function fn() {
     var a = 1;
     return a;
+  }
+  function fn2(){
+	  var b = 2;
+	  return b;
   }
 }
